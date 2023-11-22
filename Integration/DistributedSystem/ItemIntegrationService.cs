168dd75d28d8243b031ef4b6202f3b20eeb5a526
Murat Dere <muratdere90@gmail.com>
2023-11-22 13:18:53 +0200

    Initial

---
+++ Integration/DistributedSystem/ItemIntegrationService.cs
@@ -0,0 +1,47 @@
+﻿using Integration.Backend;
+using Integration.Common;
+using RedLockNet.SERedis;
+
+namespace Integration.DistributedSystem
+{
+    public class ItemIntegrationService
+    {
+        private readonly RedLockFactory _redLockFactory;
+        private ItemOperationBackend ItemIntegrationBackend { get; set; } = new();
+
+
+        public ItemIntegrationService(RedLockFactory redLockFactory)
+        {
+            _redLockFactory = redLockFactory;
+        }
+
+        public Result SaveItem(string itemContent)
+        {
+
+            /*
+             *  Notes
+             *      - Assuming Redis configurations and Dependency Injection settings have been made.
+             *      - Assuming Redis is used instead of ItemIntegrationBackend.
+             *      Since ItemIntegrationBackend stores data in InMemory, it should be stored in a common storage space instead
+             * */
+
+            using (var redLock = _redLockFactory.CreateLock(itemContent, TimeSpan.FromSeconds(30)))
+            {
+                if (redLock.IsAcquired)
+                {
+                    if (ItemIntegrationBackend.FindItemsWithContent(itemContent).Count != 0)
+                    {
+                        return new Result(false, $"Duplicate item received with content {itemContent}.");
+                    }
+
+                    var item = ItemIntegrationBackend.SaveItem(itemContent);
+
+                    return new Result(true, $"Item with content {itemContent} saved with id {item.Id}");
+                }
+
+                return new Result(false, $"Duplicate item received with content {itemContent}.");
+            }
+        }
+
+    }
+}
---
+++ Integration/Program.cs
@@ -0,0 +1,37 @@
+﻿using Integration.Service;
+
+namespace Integration;
+
+public abstract class Program
+{
+    public static void Main(string[] args)
+    {
+        var service = new ItemIntegrationService();
+
+        ThreadPool.QueueUserWorkItem(_ => service.SaveItem("c"));
+        ThreadPool.QueueUserWorkItem(_ => service.SaveItem("a"));
+        ThreadPool.QueueUserWorkItem(_ => service.SaveItem("a"));
+        ThreadPool.QueueUserWorkItem(_ => service.SaveItem("a"));
+        ThreadPool.QueueUserWorkItem(_ => service.SaveItem("a"));
+        ThreadPool.QueueUserWorkItem(_ => service.SaveItem("b"));
+
+        Thread.Sleep(500);
+
+        ThreadPool.QueueUserWorkItem(_ => service.SaveItem("a"));
+        ThreadPool.QueueUserWorkItem(_ => service.SaveItem("b"));
+        ThreadPool.QueueUserWorkItem(_ => service.SaveItem("c"));
+
+        Thread.Sleep(5000);
+
+
+        Parallel.Invoke(
+        () => service.SaveItem("Item 2"),
+        () => service.SaveItem("Item 1"),
+        () => service.SaveItem("Item 1") // This should trigger a duplicate message
+        );
+        Console.WriteLine("Everything recorded:");
+        service.GetAllItems().ForEach(Console.WriteLine);
+
+        Console.ReadLine();
+    }
+}
